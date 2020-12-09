7e912eaf6f74e63433381cd40bf1e4f8c51b2143
Michael Kong <mkong1@gmail.com>
2020-12-08 21:55:38 -0500

    lockout 3h momentary turbo

--- flashlight-firmware/anduril2/ToyKeeper/spaghetti-monster/anduril/flash_light.sh
+++ flashlight-firmware/anduril2/ToyKeeper/spaghetti-monster/anduril/flash_light.sh
@@ -8,10 +8,9 @@ usage="\nUsage: `basename $0` -fhl
   -h show this help text
   -l select the number for your flashlight (if present)
     1. D4V2
-    2. KR4
-    3. KR1
-    4. D4V2/KR4 with E21A led
-    5. D4SV2
+    2. KR4 and D4V2 with KR4 driver
+    3. KR1 and D4V2/KR4 with E21A LEDs
+    4. D4SV2
   -f hex filename (hard-coded with -p t1634, might not work with your light!)"
 
 while getopts ':hlf:' option; do
@@ -23,13 +22,13 @@ while getopts ':hlf:' option; do
          1) echo "D4V2"
            avrdude -c usbasp -p t1634 -u -Uflash:w:anduril.emisar-d4v2.hex
            exit;;
-         2) echo "KR4 or D4V2 with KR4 driver"
+         2) echo "KR4 and D4V2 with KR4 driver"
            avrdude -c usbasp -p t1634 -u -Uflash:w:anduril.noctigon-kr4.hex
            exit;;
-         3|4) echo "KR1 or D4V2/KR4 with E21A LEDs"
+         3) echo "KR1 and D4V2/KR4 with E21A LEDs"
            avrdude -c usbasp -p t1634 -u -Uflash:w:anduril.noctigon-kr4-nofet.hex
            exit;;
-         5) echo "D4SV2"
+         4) echo "D4SV2"
            avrdude -c usbasp -p t1634 -u -Uflash:w:anduril.emisar-d4sv2.hex
            exit;;
        esac
