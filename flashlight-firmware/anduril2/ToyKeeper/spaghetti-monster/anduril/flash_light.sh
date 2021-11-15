e20adf4f33e9a494f60be65dffd5a418c1e95629
Michael Kong <mkong1@gmail.com>
2021-11-15 12:21:23 -0500

    script language

--- flashlight-firmware/anduril2/ToyKeeper/spaghetti-monster/anduril/flash_light.sh
+++ flashlight-firmware/anduril2/ToyKeeper/spaghetti-monster/anduril/flash_light.sh
@@ -8,17 +8,17 @@ usage="\nUsage: `basename $0` -fhl
   -h show this help text
   -l select the number for your flashlight (if present)
     1. D4V2
-    2. KR4 and D4V2 with KR4 driver
-    3. KR1 and D4V2/KR4 with E21A LEDs
-    4. D4V2 with 219b LEDs
-    5. KR5 tintramp
+    2. D4V2/KR4 with linear driver (FET)
+    3. KR1 and D4V2/KR4 with E21A LEDs (no FET)
+    4. D4V2/KR4 with 219b LEDs (50% FET)
+    5. D4V2/KR4 tintramp
     6. D4SV2
     7. D4SV2 with tintramp
     8. MF01S
     9. BLF LT1
-    10. FW3A nofet
-    11. FW3A with 219b/c LEDs
-    12. FW3A with full FET
+    10. FW3A no FET
+    11. FW3A with 219b/c LEDs (50% FET)
+    12. FW3A (FET)
   -f hex filename (hard-coded with -p t1634, might not work with your light!)"
 
 while getopts ':hlf:' option; do
@@ -30,16 +30,16 @@ while getopts ':hlf:' option; do
          1) echo "D4V2"
            avrdude -c usbasp -p t1634 -u -Uflash:w:anduril.emisar-d4v2.hex
            exit;;
-         2) echo "KR4 and D4V2 with KR4 driver"
+         2) echo "D4V2/KR4 with linear driver (FET)"
            avrdude -c usbasp -p t1634 -u -Uflash:w:anduril.noctigon-kr4.hex
            exit;;
-         3) echo "KR1 and D4V2/KR4 with E21A LEDs"
+         3) echo "KR1 and D4V2/KR4 with E21A LEDs (no FET)"
            avrdude -c usbasp -p t1634 -u -Uflash:w:anduril.noctigon-kr4-nofet.hex
            exit;;
-         4) echo "D4V2 and KR4 with 219b"
+         4) echo "D4V2/KR4 with 219b"
            avrdude -c usbasp -p t1634 -u -Uflash:w:anduril.noctigon-kr4-219b.hex
            exit;;
-         5) echo "KR4 tintramp"
+         5) echo "D4V2/KR4 tintramp"
            avrdude -c usbasp -p t1634 -u -Uflash:w:anduril.noctigon-kr4-tintramp.hex
            exit;;
          6) echo "D4SV2"
@@ -54,13 +54,13 @@ while getopts ':hlf:' option; do
          9) echo "BLF LT1"
            avrdude -c usbasp -p t85 -u -Uflash:w:anduril.blf-lantern.hex
            exit;;
-         10) echo "FW3A nofet"
+         10) echo "FW3A no FET"
            avrdude -c usbasp -p t85 -u -Uflash:w:anduril.fw3a-nofet.hex
            exit;;
-         11) echo "FW3A 219"
+         11) echo "FW3A 219b/c (50% FET)"
            avrdude -c usbasp -p t85 -u -Uflash:w:anduril.fw3a-219.hex
            exit;;
-         12) echo "FW3A FET"
+         12) echo "FW3A (FET)"
            avrdude -c usbasp -p t85 -u -Uflash:w:anduril.fw3a.hex
            exit;;
        esac
