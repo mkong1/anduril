da104bacff4cf2ba35a3b4e32e1fc217122c132b
Michael Kong <mkong1@gmail.com>
2021-11-14 19:24:20 -0500

    d4sv2 tintramp hex

--- flashlight-firmware/anduril2/ToyKeeper/spaghetti-monster/anduril/flash_light.sh
+++ flashlight-firmware/anduril2/ToyKeeper/spaghetti-monster/anduril/flash_light.sh
@@ -11,12 +11,13 @@ usage="\nUsage: `basename $0` -fhl
     2. KR4 and D4V2 with KR4 driver
     3. KR1 and D4V2/KR4 with E21A LEDs
     4. D4SV2
-    5. MF01S
-    6. BLF LT1
-    7. D4V2 with 219b LEDs
-    8. FW3A nofet
-    9. FW3A with 219b/c LEDs
-    10. FW3A with full FET
+    5. D4SV2 with tintramp
+    6. MF01S
+    7. BLF LT1
+    8. D4V2 with 219b LEDs
+    9. FW3A nofet
+    10. FW3A with 219b/c LEDs
+    11. FW3A with full FET
   -f hex filename (hard-coded with -p t1634, might not work with your light!)"
 
 while getopts ':hlf:' option; do
@@ -37,22 +38,25 @@ while getopts ':hlf:' option; do
          4) echo "D4SV2"
            avrdude -c usbasp -p t1634 -u -Uflash:w:anduril.emisar-d4sv2.hex
            exit;;
-         5) echo "MF01S"
+         5) echo "D4SV2 tintramp"
+           avrdude -c usbasp -p t1634 -u -Uflash:w:anduril.emisar-d4sv2-tintramp.hex
+           exit;;
+         6) echo "MF01S"
            avrdude -c usbasp -p t85 -u -Uflash:w:anduril.mateminco-mf01s.hex
            exit;;
-         6) echo "BLF LT1"
+         7) echo "BLF LT1"
            avrdude -c usbasp -p t85 -u -Uflash:w:anduril.blf-lantern.hex
            exit;;
-         7) echo "D4V2 and KR4 with 219b"
+         8) echo "D4V2 and KR4 with 219b"
            avrdude -c usbasp -p t1634 -u -Uflash:w:anduril.noctigon-kr4-219b.hex
            exit;;
-         8) echo "FW3A nofet"
+         9) echo "FW3A nofet"
            avrdude -c usbasp -p t85 -u -Uflash:w:anduril.fw3a-nofet.hex
            exit;;
-         9) echo "FW3A 219"
+         10) echo "FW3A 219"
            avrdude -c usbasp -p t85 -u -Uflash:w:anduril.fw3a-219.hex
            exit;;
-         10) echo "FW3A FET"
+         11) echo "FW3A FET"
            avrdude -c usbasp -p t85 -u -Uflash:w:anduril.fw3a.hex
            exit;;
        esac
